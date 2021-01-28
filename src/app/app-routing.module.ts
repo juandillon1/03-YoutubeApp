0cf70c9b11daa1f62fbc6c0d671d962850ed5fad
Juancito <juancruzdillon123@gmail.com>
2021-01-28 14:59:48 -0300

    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { VideosComponent } from './pages/videos/videos.component';
+import { MercadopagoComponent } from './pages/mercadopago/mercadopago.component';
+import { FormulariopagoComponent } from './pages/formulariopago/formulariopago.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'videos', component: VideosComponent},
+  {path: 'mercadopago', component: MercadopagoComponent},
+  {path: 'formulario', component: FormulariopagoComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
+import { registerLocaleData } from '@angular/common';
+import localeARG from '@angular/common/locales/es-AR';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { VideosComponent } from './pages/videos/videos.component';
+import { MercadopagoComponent } from './pages/mercadopago/mercadopago.component';
+import { MercadopagoservService } from './services/mercadopagoserv.service';
+import { FormulariopagoComponent } from './pages/formulariopago/formulariopago.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+registerLocaleData(localeARG);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    VideosComponent,
+    MercadopagoComponent,
+    FormulariopagoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    MercadopagoservService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
